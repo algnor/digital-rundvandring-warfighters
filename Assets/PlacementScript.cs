069834ad4992191e792fa5aef7dbd4aac7199764
Sebastian <69853467+abbsebsia@users.noreply.github.com>
2022-03-15 10:11:27 -0700

    fixed scripts

--- Assets/PlacementScript.cs
+++ Assets/PlacementScript.cs
@@ -9,22 +9,48 @@ public class PlacementScript : MonoBehaviour
 {
     public GameObject placementIndicator;
     public GameObject Skrapan;
+    public GameObject[] Skrapor;
+    public int YearIndex;
    
     private Pose placementPose;
-    private ARRaycastManager raycastManager;
     private bool PlacementPoseIsValid = false;
+    private GameObject currentSkrapan;
+    private GameObject oldSkrapan;
 
-    List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    private ARRaycastManager raycastManager;
+    private ARSessionOrigin ARSessionOrigin;
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    private GameObject[] switchButtons;
+
+    private bool SkrapanHasBeenPlaced = false;
 
     void Start()
     {
         raycastManager = FindObjectOfType<ARRaycastManager>();
+        ARSessionOrigin = FindObjectOfType<ARSessionOrigin>();
+        switchButtons = GameObject.FindGameObjectsWithTag("switchBTN");
+        foreach (GameObject button in switchButtons)
+        {
+            button.SetActive(false);
+        }
     }
 
     void FixedUpdate()
+    {   
+        if(!SkrapanHasBeenPlaced)
+        {
+            UpdatePlacementPose();
+            UpdatePlacementIndicator();
+        }
+       
+    }
+
+    private void Update()
     {
-        UpdatePlacementPose();
-        UpdatePlacementIndicator();
+        if (SkrapanHasBeenPlaced)
+        {
+            currentSkrapan = GameObject.FindGameObjectWithTag("building");
+        }
     }
 
     private void UpdatePlacementIndicator()
@@ -61,10 +87,34 @@ public class PlacementScript : MonoBehaviour
     {
        if (hits.Count > 0)
        {
-           GameObject.Instantiate(Skrapan, placementPose.position, Quaternion.Euler(0, 0, 270));
+           GameObject.Instantiate(Skrapor[0], placementPose.position, Quaternion.Euler(0, 0, 270), ARSessionOrigin.transform);
+           SkrapanHasBeenPlaced = true;
+
+           foreach(GameObject button in switchButtons)
+           {
+              button.SetActive(true);
+           }
+
+           var PlaceElements = GameObject.FindGameObjectsWithTag("placeBTN");
+           foreach(GameObject element in PlaceElements)
+           {
+              element.SetActive(false);
+           }
+
+            placementIndicator.SetActive(false);
 
        }
-        
 
+    }
+
+    public void ChangeSkrapan(int numberInput)
+    {
+        Debug.Log(YearIndex);
+        if (YearIndex > Skrapor.Length - 1) YearIndex = 0;
+        if (YearIndex == -1) YearIndex = Skrapor.Length - 1;
+        oldSkrapan = currentSkrapan;
+        currentSkrapan = Instantiate(Skrapor[YearIndex], currentSkrapan.transform.position, currentSkrapan.transform.rotation, ARSessionOrigin.transform);
+        Destroy(oldSkrapan);
+        YearIndex += numberInput;
     }
 }
